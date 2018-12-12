7d8df013cff3f638b833eeaf6b3d823cc13b56b0
kiran <gvkk@yahoo.com>
2018-12-12 11:46:46 -0500

    jenkins java code

---
+++ src/Jenkins.java
@@ -0,0 +1,15 @@
+
+public class Jenkins {
+
+	public static void main(String[] args) {
+		
+				// TODO Auto-generated method stub
+		
+		for(int i=1;i<10;i++)
+		{
+			System.out.println("this is for testing jenkins");
+		}
+
+	}
+
+}
